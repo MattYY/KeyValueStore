60197e58935aadd9d160537b5f270b4b226cdc9d
Matthew Yannascoli <matthew.yannascoli@rga.com>
2016-04-18 09:55:15 -0400

    refactored api to included load/load-async methods

---
+++ Source/KeyValueStore+Types.swift
@@ -0,0 +1,11 @@
+//
+//  KeyValueStore+Types.swift
+//  KeyValueStore
+//
+//  Created by Matthew Yannascoli on 4/17/16.
+//  Copyright © 2016 Matthew Yannascoli. All rights reserved.
+//
+
+import UIKit
+
+
--- Source/KeyValueStore.swift
+++ Source/KeyValueStore.swift
@@ -9,60 +9,120 @@
 import Foundation
 
 
+
+///
+///
+///
+///
 public class KeyValueStore {
     private let directoryURL: NSURL
     private let plistName: String
-    private let queue = dispatch_queue_create("com.keyvaluestore", DISPATCH_QUEUE_SERIAL)
+    
+    //Keep an `isLoaded` flag to ensure the local `data` object is populated with
+    //the data from the backing file. (We cannot use nil state for data to represent this
+    //because it would (legitimately) because data will be nil if nothing has yet been saved to it.
+    private var isLoaded: Bool = false
+    
+    private let queue: NSOperationQueue = {
+        let queue = NSOperationQueue()
+        queue.qualityOfService = .Background
+        return queue
+    }()
     
     private var fileURL: NSURL {
         return directoryURL.URLByAppendingPathComponent("/\(plistName).plist")
     }
-    
-    ///
-    private(set) internal var data: [String: AnyObject]?
+
+    //
+    private var data: [String: AnyObject] = [:]
     
     ///
     public enum KeyValueStoreError: ErrorType, CustomDebugStringConvertible {
-        case InvalidFilePath(path: String)
+        case InvalidFilePath(string: String)
+        case BackingFileError(error: NSError)
+        
         public var debugDescription: String {
             switch self {
             case .InvalidFilePath(let path):
-                return "Archived data cannot be found at file path: \(path)"
+                return "Unable to create file at path: \(path)"
+            case .BackingFileError(let error):
+                return "Unable to create backing file with error: \(error.localizedDescription)"
             }
         }
     }
     
     
-    ///Instantiate a base instance
+    /// Instantiate a base instance
     /// - directoryURL
     /// - plistName
     /// - logOutput
     public required init(directoryURL: NSURL, plistName: String, logOutput: Bool = false) throws {
-        guard let path = directoryURL.relativePath else {
-            throw KeyValueStoreError.InvalidFilePath(path: "nil")
-        }
-
-        //verify the passed in directoryURL is writeable/readable
-        let fileManager = NSFileManager.defaultManager()
-        let isWritable = fileManager.isWritableFileAtPath(path)
-        let isReadable = fileManager.isReadableFileAtPath(path)
-        if !isWritable || !isReadable {
-            throw KeyValueStoreError.InvalidFilePath(path: path)
-        }
-        
         self.directoryURL = directoryURL
         self.plistName = plistName
         
-        //Synchronously fetch file contents from disk
-        data = NSDictionary(contentsOfURL: fileURL) as? [String: AnyObject]
+        try createContainerDirectoryIfNecessary()
         
         //Logging
         loggingOn = logOutput
     }
     
     
-    /// Deletes the file in which key-value data is stored.
-    public func deleteDataFile() throws {
+    private func createContainerDirectoryIfNecessary() throws {
+        guard let path = directoryURL.relativePath else {
+            throw KeyValueStoreError.InvalidFilePath(string: "nil")
+        }
+        
+        let fileManager = NSFileManager.defaultManager()
+        if(!fileManager.fileExistsAtPath(path)) {
+            do {
+                try fileManager.createDirectoryAtURL(self.fileURL, withIntermediateDirectories: true, attributes: nil)
+            }
+            catch let error as NSError {
+                Log("Unable to create backing file with error: \(error.localizedDescription)")
+                throw KeyValueStoreError.BackingFileError(error: error)
+            }
+        }
+    }
+}
+
+
+
+
+
+//MARK: - Api -
+extension KeyValueStore {
+    ///Synchronously fetch file contents from disk
+    public func load() {
+        data = (NSDictionary(contentsOfURL: fileURL) as? [String: AnyObject]) ?? [:]
+        isLoaded = true
+    }
+    
+    ///
+    public func load(completion: (() -> Void)?) {
+        queue.addOperationWithBlock {
+            let fileData = (NSDictionary(contentsOfURL: self.fileURL) as? [String: AnyObject]) ?? [:]
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                self.data = fileData
+                self.isLoaded = true
+                completion?()
+            }
+        }
+    }
+    
+    ///
+    public func unload() {
+        data = [:]
+        isLoaded = false
+    }
+    
+    
+    /// Synchronously deletes the file in which key-value data is
+    /// stored after canceling any enqued safe operations.
+    public func delete() throws {
+        queue.cancelAllOperations()
+        unload()
+        
         do {
             try NSFileManager.defaultManager().removeItemAtURL(fileURL)
         }
@@ -70,17 +130,52 @@ public class KeyValueStore {
             throw error
         }
     }
+    
+    
+    ///
+    public func setValue(value: AnyObject?, forKey key: String) {
+        guard isLoaded else {
+            Log("You must call one of the `load` methods before setting a new value on the store.")
+            return
+        }
+        
+        self.data[key] = value
+        self.saveToDisk()
+    }
+    
+    ///
+    public func valueForKey(key: String) -> AnyObject? {
+        guard isLoaded else {
+            Log("Attempting to get a value on a store that is not loaded.")
+            return nil
+        }
+        
+        return data[key]
+    }
+    
+    
+    private func saveToDisk() {
+        guard isLoaded else {
+            Log("Cannot save to backing file on a store that is not in a loaded state.")
+            return
+        }
+
+        queue.addOperationWithBlock {
+            (self.data as NSDictionary).writeToURL(self.fileURL, atomically: true)
+        }
+    }
 }
 
 
-///Getters/Setters
+
+
+//MARK: - Convenience Accessors -
 extension KeyValueStore {
-    
     ///Set a String
     public func setString(string: String, forKey key: String) {
         self.setValue((string as NSString), forKey: key)
     }
-
+    
     ///Retrieve a String
     public func stringForKey(key: String) -> String? {
         return self.valueForKey(key) as? String
@@ -118,7 +213,7 @@ extension KeyValueStore {
         let value = self.valueForKey(key) as? NSNumber
         return value?.doubleValue
     }
-
+    
     ///Set a bool
     public func setBool(bool: Bool, forKey key: String) {
         self.setValue(NSNumber(bool: bool), forKey: key)
@@ -133,7 +228,7 @@ extension KeyValueStore {
     public func setDate(date: NSDate?, forKey key: String) {
         self.setValue(date, forKey: key)
     }
-
+    
     ///Retrieve a date
     public func dateForKey(key: String) -> NSDate? {
         return self.valueForKey(key) as? NSDate
@@ -141,56 +236,6 @@ extension KeyValueStore {
 }
 
 
-
-
-
-extension KeyValueStore {
-    private func saveToDisk() {
-        guard let data = self.data else {
-            Log("Save to disk aborted because there is no data to save")
-            return
-        }
-        
-        guard let path = self.fileURL.relativePath else {
-            Log(KeyValueStoreError.InvalidFilePath(path: "\(fileURL)").debugDescription)
-            return
-        }
-        
-        dispatch_async(queue) {
-            let fileManager = NSFileManager.defaultManager()
-            if(!fileManager.fileExistsAtPath(path)) {
-                do {
-                    try fileManager.createDirectoryAtURL(self.fileURL, withIntermediateDirectories: true, attributes: nil)
-                }
-                catch let error as NSError {
-                    Log("Unable to create file with error: \(error.localizedDescription)")
-                }
-                
-                (data as NSDictionary).writeToURL(self.fileURL, atomically: true)
-            }
-        }
-    }
-    
-    //Sync
-    private func setValue(value: AnyObject?, forKey key: String) {
-        if self.data != nil  {
-            self.data![key] = value
-        }
-        else {
-            //no data is current stored so create a fresh
-            //dictionary if the applied value is not nil
-            if let value = value {
-                self.data = [key : value]
-            }
-        }
-        
-        self.saveToDisk()
-    }
-    
-    private func valueForKey(key: String) -> AnyObject? {
-        return data?[key]
-    }
-}
 
 
 //MARK: - Log -
